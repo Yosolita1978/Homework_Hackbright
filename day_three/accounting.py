ae7c986e31f319383649cb9587c8c0702480c113
Cristina Rodriguez <yosola@gmail.com>
2017-01-04 20:48:57 -0800

    Homework day three

---
+++ day_three/accounting.py
@@ -0,0 +1,25 @@
+def find_paid_costumer(filename):
+    my_file = open(filename)
+    melon_cost = 1.00
+    for line in my_file:
+        line = line.rstrip()
+        words = line.split('|')
+
+        username = words[1]
+        amount = int(words[2])
+        userpaid = float(words[3])
+        
+        paid_expected = amount * melon_cost
+
+
+        if paid_expected < userpaid:
+            print "Overpaid", username, "paid {:.2f}, expected {:.2f}".format(
+                userpaid, paid_expected)
+        
+        elif paid_expected > userpaid:
+            print username, "paid {:.2f}, expected {:.2f}".format(
+                userpaid, paid_expected)
+
+
+find_paid_costumer("customer-orders.txt")
+
